3e8a77522ac775e1d8863096e0626797f9f90863
haitham tory <haithamtory@haithams-mac.local>
2016-05-13 06:31:06 -0700

    Initial Commit

---
+++ UploadPhotoAlamofire/NetData.swift
@@ -0,0 +1,59 @@
+//
+//  NetData.swift
+//  UploadPhotoAlamofire
+//
+//  Created by haitham tory on 5/13/16.
+//  Copyright © 2016 haitham tory. All rights reserved.
+//
+
+import UIKit
+
+enum MimeType: String {
+    case ImageJpeg = "image/jpeg"
+    case ImagePng = "image/png"
+    case ImageGif = "image/gif"
+    case Json = "application/json"
+    case Unknown = ""
+    
+    func getString() -> String? {
+        switch self {
+        case .ImagePng:
+            fallthrough
+        case .ImageJpeg:
+            fallthrough
+        case .ImageGif:
+            fallthrough
+        case .Json:
+            return self.rawValue
+        case .Unknown:
+            fallthrough
+        default:
+            return nil
+        }
+    }
+}
+
+class NetData: NSObject {
+
+    let data: NSData
+    let mimeType: MimeType
+    let filename: String
+    
+    init(data: NSData, mimeType: MimeType, filename: String) {
+        self.data = data
+        self.mimeType = mimeType
+        self.filename = filename
+    }
+    
+    init(pngImage: UIImage, filename: String) {
+        data = UIImagePNGRepresentation(pngImage)!
+        self.mimeType = MimeType.ImagePng
+        self.filename = filename
+    }
+    
+    init(jpegImage: UIImage, compressionQuanlity: CGFloat, filename: String) {
+        data = UIImageJPEGRepresentation(jpegImage, compressionQuanlity)!
+        self.mimeType = MimeType.ImageJpeg
+        self.filename = filename
+    }
+}
